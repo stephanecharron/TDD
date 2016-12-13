3eb299ccac82c6f8d2e2188dd2ab438ed2b10e29
stephanecharron <stephanecharron@this.com>
2016-12-12 21:47:18 -0500

    $apply impl.

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -386,4 +386,32 @@ describe('Scope', function () {
             expect(result).toBe(44);
         });
     });
+
+    describe('$apply', function () {
+        var scope;
+
+        beforeEach(function () {
+            scope = new Scope();
+        });
+
+        it('executes the given function and starts the digest', function () {
+           scope.aValue = 'abc';
+            scope.counter = 0;
+
+            scope.$watch(function (scope) {
+                return scope.aValue;
+            }, function (newValue, oldValue, scope) {
+                scope.counter++;
+            });
+
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+
+            scope.$apply(function (scope) {
+               scope.aValue = 'a';
+            });
+            expect(scope.counter).toBe(2);
+
+        });
+    });
 });
